3f1ecaabc1172d340435b040a764679ce3fc3174
Marc Wolpert <mewolpert@gmail.com>
2024-11-25 22:04:49 -0800

    feat: Added basic styling for the todo list

--- todo_list/src/index.js
+++ todo_list/src/index.js
@@ -3,12 +3,3 @@ import "./template.html";
 import {appendHomepage, addHomeEventListeners} from "./lib/homeTab.js";
 import {appendMenu, addMenuEventListener, appendMenuButton} from "./lib/menuTab.js";
 import {appendContact,addContactEventListener} from "./lib/contactTab.js";
-
-
-const content=document.getElementById("content");
-
-appendHomepage(content);
-addHomeEventListeners(content);
-addMenuEventListener(content);
-addContactEventListener(content);
-
